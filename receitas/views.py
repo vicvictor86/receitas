111465cd7386cd034b058c357226a440b3a1e5df
Victor <vicbem@hotmail.com>
2021-11-13 15:01:03 -0300

    Landing page finalizada

---
+++ receitas/views.py
@@ -0,0 +1,22 @@
+from receitas.models import Receita
+from django.shortcuts import get_object_or_404, render
+from .models import Receita
+
+# Create your views here.
+def index(request):
+    receitas = Receita.objects.all()
+
+    dados = {
+        'receitas' : receitas
+    }
+
+    return render(request,'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita' : receita
+    }
+
+    return render(request, 'receita.html' , receita_a_exibir)
